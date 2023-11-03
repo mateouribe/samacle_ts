a215c5362d271791385442da25426ce5206ec35f
Mateo Arismendy <arismendyuribemateo@gmail.com>
2023-11-02 21:02:47 -0400

    add projects page

---
+++ src/components/projects/item/index.tsx
@@ -0,0 +1,140 @@
+import { useLayoutEffect, useRef } from "react";
+import { Expo, gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { Link } from "react-router-dom";
+
+type Props = {
+  project: Project;
+  index: number;
+};
+
+const Item = ({ project, index }: Props) => {
+  const container = useRef(null);
+  const tl = useRef(null || gsap.timeline());
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      if (index !== 0) {
+        ScrollTrigger.create({
+          trigger: container.current,
+          start: "top 85%",
+          end: "bottom 85%",
+          animation: gsap.fromTo(
+            ".projectContainer",
+            {
+              opacity: 0,
+              yPercent: 100,
+            },
+            {
+              opacity: 1,
+              yPercent: 0,
+              duration: 1.5,
+              ease: Expo.easeOut,
+            }
+          ),
+        });
+      }
+
+      tl.current = gsap.timeline({ paused: true });
+
+      tl.current.to(".projectContainer", {
+        scale: 0.99,
+        duration: 2,
+        ease: Expo.easeOut,
+      });
+    }, container);
+
+    return () => ctx.revert();
+  }, [index]);
+
+  return (
+    <figure
+      key={index}
+      onMouseEnter={() => {
+        if (project.completed) {
+          tl.current.play();
+          tl.current.duration(2);
+        }
+      }}
+      onMouseLeave={() => {
+        if (project.completed) {
+          tl.current.reverse();
+          tl.current.duration(0.8);
+        }
+      }}
+    >
+      <Link
+        className={`w-full h-[80vh] flex flex-col gap-10 hoverMouse cursor-none ${
+          index === 0 && "animateFirstProject"
+        }`}
+        ref={container}
+        to={
+          project.completed
+            ? `/our-projects/study-case/${project.info.link}`
+            : "#"
+        }
+      >
+        <div className="relative flex w-full h-full gap-20 projectContainer">
+          {/* image left */}
+          <div
+            className="w-full h-full md:w-1/2"
+            style={{
+              backgroundImage: `url(${project.mockupImages.big})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          />
+
+          {/* images right */}
+          <div className="flex-col hidden w-1/2 h-full gap-20 md:flex">
+            <div
+              className={`w-full h-[60%] ${
+                index % 2 === 0 ? "order-2" : "order-1"
+              }`}
+              style={{
+                backgroundImage: `url(${project.mockupImages.mid})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            />
+            <div
+              className={`w-full h-[40%] ${
+                index % 2 === 0 ? "order-1" : "order-2"
+              }`}
+              style={{
+                backgroundImage: `url(${project.mockupImages.sm})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            />
+          </div>
+
+          {!project.completed && (
+            <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full bg-black/80">
+              <span className="text-[20px] md:text-[30px] text-gray">
+                Coming soon...
+              </span>
+            </div>
+          )}
+        </div>
+        <figcaption className="flex justify-between">
+          <h5 className="text-black textProject">
+            {project.info.title} - {project.info.place}
+          </h5>
+          <div className="flex flex-col">
+            {project.info.tags.map((tag, index) => (
+              <span className="text-xsm text-gray textProject" key={index}>
+                {tag}
+              </span>
+            ))}
+          </div>
+        </figcaption>
+      </Link>
+    </figure>
+  );
+};
+
+export default Item;
--- src/screens/projects/index.tsx
+++ src/screens/projects/index.tsx
@@ -1,9 +1,139 @@
-import React from "react";
+import { useLayoutEffect, useRef } from "react";
+import Section from "../../components/customElements/section";
+import SectionTitle from "../../components/customElements/sectionTitle";
+import { colors, projects } from "../../utils/constants";
+import { Expo, gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet-async";
+import Item from "../../components/projects/item";
+gsap.registerPlugin(ScrollTrigger);
 
-type Props = {};
+const Projects = () => {
+  const container = useRef(null);
+  const { t } = useTranslation();
 
-const Projects = (props: Props) => {
-  return <div>Projects</div>;
+  useLayoutEffect(() => {
+    const itemsTitle: string[][] = [];
+
+    // Split title
+    gsap.utils.toArray(".projectsPageTitle").forEach((el) => {
+      const splitTitleParent = new (window as any).SplitText(el, {
+        type: "words",
+        wordsClass: "wordsParent",
+      });
+
+      const splitTitle = new (window as any).SplitText(el, {
+        type: "words",
+        wordsClass: "wordsBlack",
+      });
+
+      itemsTitle.push(splitTitle.words);
+    });
+
+    //Split text
+    const splitTextParent = new (window as any).SplitText(".projectPageText", {
+      type: "words",
+      wordsClass: "wordsParent",
+    });
+
+    const splitText = new (window as any).SplitText(".projectPageText", {
+      type: "words",
+      wordsClass: "wordsBlack",
+    });
+
+    const body = document.querySelector("body");
+
+    const ctx = gsap.context(() => {
+      gsap.set(".animateFirstProject", {
+        opacity: 0,
+        yPercent: 100,
+      });
+
+      const tl = gsap.timeline({});
+
+      tl.fromTo(
+        itemsTitle,
+        {
+          y: 48,
+        },
+        {
+          y: 0,
+          duration: 1.5,
+          stagger: 0.1,
+          ease: Expo.easeOut,
+        }
+      );
+      tl.fromTo(
+        splitText.words,
+        {
+          y: 48,
+        },
+        {
+          y: 0,
+          duration: 1.5,
+          stagger: 0.01,
+          ease: Expo.easeOut,
+        },
+        "-=1.7"
+      );
+      tl.to(
+        ".animateFirstProject",
+        {
+          opacity: 1,
+          yPercent: 0,
+          duration: 1.5,
+          ease: Expo.easeOut,
+        },
+        "-=1.5"
+      );
+
+      gsap.to(body, {
+        backgroundColor: colors.white,
+      });
+      //scroll to top right before it mounts
+      window.scrollTo(0, 0);
+    }, container);
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <main ref={container}>
+      <Helmet>
+        <title>{t("seo.projects.title")}</title>
+        <meta name="description" content={t("seo.projects.metaDescription")} />
+        <link rel="canonical" href="/our-projects" />
+      </Helmet>
+      <Section
+        hasPadding
+        className="flex flex-col py-desktop md:py-desktop gap-100"
+      >
+        <div className="flex items-center justify-between w-full">
+          <div>
+            <SectionTitle
+              className="text-black italic leading-[100%] projectsPageTitle"
+              text={t("projects.welcomeMessage.one")}
+            ></SectionTitle>
+            <SectionTitle
+              className="text-black italic leading-[100%] projectsPageTitle"
+              text={t("projects.welcomeMessage.two")}
+            ></SectionTitle>
+            <SectionTitle
+              className="text-black italic leading-[100%] projectsPageTitle"
+              text={t("projects.welcomeMessage.three")}
+            ></SectionTitle>
+          </div>
+          <p className="w-[70%] md:w-[50%] lg:w-[30%] text-black projectPageText leading-[90%]">
+            {t("projects.welcomeMessage.text")}
+          </p>
+        </div>
+        {projects.map((project, index) => (
+          <Item project={project} index={index} key={index} />
+        ))}
+      </Section>
+    </main>
+  );
 };
 
 export default Projects;
